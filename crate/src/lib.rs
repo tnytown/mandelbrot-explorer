86670b1450c36515e4960827e06de096e1645d10
Andrew Pan <known@unown.me>
2019-05-22 00:12:44 -0500

    initial crack at UI, gradient coloring

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -2,6 +2,7 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen::Clamped;
 use web_sys::console;
 use web_sys::{CanvasRenderingContext2d, ImageData};
+use palette::{LinSrgb, Gradient};
 
 use num_complex::Complex;
 
@@ -21,9 +22,15 @@ pub fn run() -> Result<(), JsValue> {
 const MAX_ITER: u32 = 1000;
 #[wasm_bindgen]
 pub fn render(ctx: &CanvasRenderingContext2d,
+              escape: u32, greyscale: bool,
               width: u32, height: u32,
               x_min: f32, x_max: f32,
               y_min: f32, y_max: f32) {
+    let grad = Gradient::new(vec![
+        LinSrgb::new(0.0, 0.0, 0.0),
+        LinSrgb::new(0.0, 1.0, 1.0),
+    ]);
+    
     let mut data = Vec::with_capacity((4 * height*width) as usize); // RGBA for h*w pixels
     for i in 0..height {
         for j in 0..width { // for all (i, j) pixels
@@ -35,16 +42,27 @@ pub fn render(ctx: &CanvasRenderingContext2d,
             let mut z = Complex::new(0.0, 0.0);
             let c = Complex::new(re, im); // get complex num c from parts
             let mut iter = 0;
-            while z.norm() < 2.0 && iter < MAX_ITER {
+            while z.norm() < 2.0 && iter < escape {
                 z = z.powf(2.0) + c; // z_(n+1) = z_n^2+c
                 iter = iter + 1
             }
+
+            let scale = (iter / (escape - 1)) * (u32::max_value()-1);
+            let mult = (iter as f32) / (escape as f32);
             
             // colors
-            data.push((iter / 4) as u8); // r
-            data.push((iter / 2) as u8); //1
-            data.push(iter as u8); // b
-            data.push(255); // a - no transparency
+            if !greyscale {
+                let (r, g, b) = grad.get(mult).into_components();
+                data.push((r * 255.0) as u8); // r
+                data.push((g * 255.0) as u8); // g
+                data.push((b * 255.0) as u8); // b
+                data.push(255); // a - no transparency
+            } else {
+                data.push(mult as u8);
+                data.push(mult as u8);
+                data.push(mult as u8);
+                data.push(255);
+            }
         }
     }
 
--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let params = {
+window.state = {
     bounds: {
 	ymin: -1,
 	ymax: 1,
@@ -12,14 +12,38 @@ import("../crate/pkg").then(module => {
     window.module = module; // make accessible from devtools
     
     module.run(); // set_panic_hook
-    params.canvas = document.getElementById("mandelbrot").getContext("2d");
+    window.state.canvas = document.getElementById("mandelbrot").getContext("2d");
     redraw();
 });
 
+function readParams() {
+    return {
+	escape: numberById("escape"),
+	greyscale: document.querySelector("input[name=color]:checked").id == "greyscale",
+	
+	bounds: {
+	    ymin: numberById("y_min"),
+	    ymax: numberById("y_max"),
+	    xmin: numberById("x_min"),
+	    xmax: numberById("x_max"),
+	},
+    };
+}
+window.readParams = readParams;
+
+function numberById(id) {
+    return document.getElementById(id).valueAsNumber;
+}
+
 function redraw() {
-    window.module.render(params.canvas, params.canvas.canvas.width, params.canvas.canvas.height,
+    let params = readParams();
+    window.module.render(state.canvas,
+			 params.escape,
+			 params.greyscale,
+			 state.canvas.canvas.width, state.canvas.canvas.height,
 			 params.bounds.xmin, params.bounds.xmax,
 			 params.bounds.ymin, params.bounds.ymax);
 }
+window.redraw = redraw;
 
 
