d125cb8128561e7552865997a7f2a0aded16fa8d
Andrew Pan <known@unown.me>
2019-05-21 19:47:05 -0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,28 @@
+let params = {
+    bounds: {
+	ymin: -1,
+	ymax: 1,
+	xmin: -2.5,
+	xmax: 1,
+    },
+    canvas: null,
+};
+
+import("../crate/pkg").then(module => {
+    window.module = module; // make accessible from devtools
+    
+    module.run(); // set_panic_hook
+    params.canvas = document.getElementById("mandelbrot").getContext("2d");
+    redraw();
+});
+
+function redraw() {
+    params.canvas.canvas.width = 640;
+    params.canvas.canvas.height = 480;
+    
+    window.module.render(params.canvas, params.canvas.canvas.width, params.canvas.canvas.height,
+			 params.bounds.xmin, params.bounds.xmax,
+			 params.bounds.ymin, params.bounds.ymax);
+}
+
+
